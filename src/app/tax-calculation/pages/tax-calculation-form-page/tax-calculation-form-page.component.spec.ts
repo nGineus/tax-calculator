deaf46dc24c96ba7be0d70a45b86f10865003e31
Oleksandr Kutsenko <oleksandr_kutsenko@epam.com>
2024-04-22 11:13:35 +0300

    Added Tests

--- src/app/tax-calculation/pages/tax-calculation-form-page/tax-calculation-form-page.component.spec.ts
+++ src/app/tax-calculation/pages/tax-calculation-form-page/tax-calculation-form-page.component.spec.ts
@@ -2,14 +2,21 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TaxCalculationFormPageComponent } from './tax-calculation-form-page.component';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { Router } from '@angular/router';
+import { TaxRoutes } from '../../constants/tax-routes';
+import { By } from '@angular/platform-browser';
 
 describe('TaxCalculationFormPageComponent', () => {
   let component: TaxCalculationFormPageComponent;
   let fixture: ComponentFixture<TaxCalculationFormPageComponent>;
 
+  let routerSpy: jasmine.SpyObj<Router>;
+
   beforeEach(async () => {
+    routerSpy = jasmine.createSpyObj<Router>('router', ['navigate']);
     await TestBed.configureTestingModule({
       declarations: [TaxCalculationFormPageComponent],
+      providers: [{ provide: Router, useValue: routerSpy }],
       schemas: [NO_ERRORS_SCHEMA],
     }).compileComponents();
 
@@ -22,5 +29,32 @@ describe('TaxCalculationFormPageComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  // TODO add tests
+  describe('calculateTaxes', () => {
+    it('should navigate to tax result page with gross annual salary in URL', () => {
+      const annualSalary = 1000;
+
+      component.calculateTaxes(annualSalary);
+
+      expect(routerSpy.navigate).toHaveBeenCalledWith([
+        TaxRoutes.CALCULATION,
+        annualSalary,
+      ]);
+    });
+
+    describe('UI', () => {
+      it('should navigate to tax result page with gross annual salary in URL WHEN salary is changed', () => {
+        const annualSalary = 1000;
+
+        const comp = fixture.debugElement.query(By.css('app-tax-input'));
+
+        comp.triggerEventHandler('valueChanged', annualSalary);
+        fixture.detectChanges();
+
+        expect(routerSpy.navigate).toHaveBeenCalledWith([
+          TaxRoutes.CALCULATION,
+          annualSalary,
+        ]);
+      });
+    });
+  });
 });
